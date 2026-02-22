c9c1ae67e20ac668d9634a9826f1f3e060d1ced7
mmalensek <marto3304@gmail.com>
2026-02-22 09:32:07 +0100

    Debugging

--- I2CLCD1602.py
+++ I2CLCD1602.py
@@ -14,9 +14,7 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 if not API_KEY:
     print("Warning: API_KEY not found")
-LJLJ = 'EDDM' # change later to LJLJ
-
-print(API_KEY)
+LJLJ = 'LJLJ' # change later to LJLJ
 
 # get system time
 def get_time_now():
